d71040bc46a983ca81ac7673444088ad83d4e47e
Newton Garcia <newxhost@gmail.com>
2018-07-26 23:08:54 -0300

    mount Tus::Server on rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # https://github.com/janko-m/tus-ruby-server
+  mount Tus::Server => "/files"
+
   get "uploads",     to: "uploads#index"
   get "uploads/new", to: "uploads#new"
   post "uploads",    to: "uploads#create"
