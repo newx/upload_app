24f67e2a59e9e0b5a7ccdb122adada14be93242c
Newton Garcia <newxhost@gmail.com>
2018-07-27 17:42:27 -0300

    wip

---
+++ app/assets/javascripts/direct_upload.js
@@ -0,0 +1,44 @@
+function fileUpload(fileInput) {
+  fileInput.style.display = "none" // uppy will add its own file input
+
+  var uppy = Uppy.Core({
+      id: fileInput.id,
+    })
+    .use(Uppy.FileInput, {
+      target: fileInput.parentNode,
+    })
+    .use(Uppy.Informer, {
+      target: fileInput.parentNode,
+    })
+    .use(Uppy.ProgressBar, {
+      target: imagePreview.parentNode,
+    })
+
+  uppy.use(Uppy.Tus, {
+    endpoint: "/files",
+    chunkSize: 5*1024*1024, // required unless tus-ruby-server is running on Goliath
+  })
+
+  uppy.on("upload-success", function (file, data) {
+    // construct uploaded file data from the tus URL
+    var uploadedFileData = JSON.stringify({
+      id: data.url,
+      storage: "cache",
+      metadata: {
+        filename:  file.name,
+        size:      file.size,
+        mime_type: file.type,
+      }
+    })
+
+    // set hidden field value to the uploaded file data so that it"s submitted with the form as the attachment
+    var hiddenInput = fileInput.parentNode.querySelector(".upload-hidden")
+    hiddenInput.value = uploadedFileData
+  })
+
+  return uppy
+}
+
+document.querySelectorAll(".upload-file").forEach(function (fileInput) {
+  fileUpload(fileInput)
+})
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,9 +1,24 @@
 class UploadsController < ApplicationController
+  before_action :build_app_file, only: %i(new create)
+
   def index
 
   end
 
   def new
-    @app_file = AppFile.new
+  end
+
+  def create
+    @app_file.save!
+  end
+
+  private
+
+  def build_app_file
+    @app_file = AppFile.new(app_file_params)
+  end
+
+  def app_file_params
+    params.fetch(:app_file, {}).permit(%i(file_data))
   end
 end
